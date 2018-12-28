baf21c235196ad48337c74c5f91c803ef8eb3c3c
Kell Skog <kell.skog@gmail.com>
2018-12-28 09:39:30 +0100

    Start first assessment

---
+++ stats.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <Add File Name> 
+ * @brief <Add Brief Description Here >
+ *
+ * <Add Extended Description Here>
+ *
+ * @author <Add FirsName LastName>
+ * @date <Add date >
+ *
+ */
+
+
+
+
+
+/* Add other Implementation File Code Here */
